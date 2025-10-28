b26933141f92c28a3f082f36682269e74dcaad5d
Balázs Szilágyi <70139847+SzilBalazs@users.noreply.github.com>
2025-10-28 17:06:41 +0100

    Fix a few more problems

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import torch
 class Model(torch.nn.Module):
     def __init__(self, dim, category_count, hidden_count1, hidden_count2):
         super().__init__()
+        self.dim = dim
+        self.category_count = category_count
         self.l1 = torch.nn.Linear(dim, hidden_count1)
         self.l2 = torch.nn.Linear(hidden_count1, hidden_count2)
         self.l3 = torch.nn.Linear(hidden_count2, dim*category_count)
@@ -15,4 +17,5 @@ class Model(torch.nn.Module):
         X = self.relu(X)
         X = self.l3(X)
 
-        return X[:, self.d, self.cat]
+        return X.view(-1, self.dim, self.category_count)
+
