bf9394d09e7b207ed196657122ccc8c22fddd11b
f <f>
2025-10-28 15:57:34 +0000

    Data generation

---
+++ data_generation.py
@@ -0,0 +1,13 @@
+import torch
+import copy
+import itertools
+
+def generate_seq(length):
+    idxs = torch.arange(length).tolist()
+
+    combs = list(itertools.combinations(idxs, length // 2))
+
+    data = torch.zeros(len(combs), 1, length)
+    data[torch.arange(len(combs))[:, None], [0], combs] = 1
+
+    return data
