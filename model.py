df3d3958d1b0fc88e102a7ca4a566e68d3e8c9e0
Balázs Szilágyi <70139847+SzilBalazs@users.noreply.github.com>
2025-10-28 16:55:51 +0100

    Update model

--- model.py
+++ model.py
@@ -1,24 +1,19 @@
 import torch
 
 class Model(torch.nn.Module):
-    def __init__(self, d, dhid1, dhid2):
+    def __init__(self, d, cat, dhid1, dhid2):
         super().__init__()
         self.l1 = torch.nn.Linear(d, dhid1)
         self.l2 = torch.nn.Linear(dhid1, dhid2)
-        self.l3 = torch.nn.Linear(dhid2, d)
+        self.l3 = torch.nn.Linear(dhid2, d*cat)
         self.relu = torch.nn.ReLU()
 
-    def forward(self, x):
-        X = self.l1(x)
+    def forward(self, X):
+        X = self.l1(X)
         X = self.relu(X)
         X = self.l2(X)
         X = self.relu(X)
         X = self.l3(X)
-        X = self.relu(X)
-
-        mask = torch.isclose(x, torch.ones_like(x)*2) == False
-
-        X[mask] = x[mask]
 
         return X
     
