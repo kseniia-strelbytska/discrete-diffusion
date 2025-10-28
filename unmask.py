b327357663b757f3bd4c13c2f95ae41775d7f424
Vince <vince.ungar@gmail.com>
2025-10-28 16:58:50 +0100

    add basic unmask step

---
+++ unmask.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class Unmasker(nn.Module):
+    def __init__(self, model):
+        super(Unmasker, self).__init__()
+        self.model = model
+
+    def forward(self, X, alpha=0.1):
+        y_pred = self.model(X)
+
+        mask = torch.rand_like(X) < alpha
+        X_unmasked = X.clone()
+        X_unmasked[mask] = y_pred[mask].argmax(dim=1)
+        return X_unmasked
+        
