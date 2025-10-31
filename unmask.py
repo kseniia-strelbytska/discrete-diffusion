2304ffa2030caf4338e58bc423e9a94616435231
Balázs Szilágyi <70139847+SzilBalazs@users.noreply.github.com>
2025-10-31 12:58:23 +0100

    Add sequential unmasker.

--- unmask.py
+++ unmask.py
@@ -2,15 +2,22 @@ import torch
 import torch.nn as nn
 
 class Unmasker(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model, alpha):
         super(Unmasker, self).__init__()
         self.model = model
+        self.alpha = alpha
 
-    def forward(self, X, alpha=0.1):
+    def forward(self, X):
         y_pred = self.model(X)
 
-        mask = torch.rand_like(X) < alpha
+        mask = torch.rand_like(X) < self.alpha
 
         X_unmasked = X.clone()
         X_unmasked[torch.isclose(X, 2) & mask] = y_pred[torch.isclose(X, 2) & mask].argmax(dim=2)
         return X_unmasked
+    
+def get_unmasker(model):
+    return nn.Sequential(
+        *[Unmasker(model, 0.1) for _ in range(40)],
+        Unmasker(model, 1.0)
+    )
